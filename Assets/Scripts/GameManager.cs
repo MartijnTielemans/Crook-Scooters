ea46538cbd63d97b9edc9d238b6db29f68aa5e4d
Martijn <martijn.tielemans@live.nl>
2021-10-26 13:17:33 +0200

    Game difficulty now scales with time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] MoveObjectScript moveScript;
+    Vector3 originalMoveSpeed;
+
     [SerializeField] Transform middle;
     [SerializeField] GameObject chunkParent;
 
@@ -13,20 +16,36 @@ public class GameManager : MonoBehaviour
     [Header("Configurables")]
     [SerializeField] int levelStartLength;
     [SerializeField] float distanceSpawnChunk = 200f;
-    public int obstacleSpawnChance = 4;
+    public float gameSpeed = 1;
+    [SerializeField] float speedIncreaseAmount = .2f;
+    [SerializeField] float maxSpeed = 3;
+    public int emptyChunkSpawnChance = 19;
+    [SerializeField] int spawnChanceIncreaseAmount = 2;
+    [SerializeField] int maxEmptySpawnChance = 27;
+    [SerializeField] float timeInterval = 12;
+    float currentTimeMilestone;
 
     void Awake()
     {
+        originalMoveSpeed = moveScript.moveSpeed;
+        currentTimeMilestone = timeInterval;
         SpawnLevelStart();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If the elapsed time has been reached
+        if (Time.time >= currentTimeMilestone)
+        {
+            ChangeGameIntensity();
+        }
+
+        // If the middle point got close enough to the last spawned chunk
         if (Vector3.Distance(middle.position, spawnedChunks[spawnedChunks.Count -1].transform.position) < distanceSpawnChunk)
         {
             // If in the range of 10, spawns any random chunk
-            int rnd = Random.Range(0, obstacleSpawnChance);
+            int rnd = Random.Range(0, emptyChunkSpawnChance);
             if (rnd >= 0 && rnd <= 10)
             {
                 SpawnChunk(RandomChunkNumber(), spawnedChunks[spawnedChunks.Count - 1].transform.Find("SpawnPoint").transform.position);
@@ -62,5 +81,31 @@ public class GameManager : MonoBehaviour
     int RandomChunkNumber()
     {
         return Random.Range(0, levelParts.Length);
+    }
+
+    void ChangeGameIntensity()
+    {
+        Debug.Log("Time milestone reached");
+
+        // Decrease obstacle spawn chance
+        if (emptyChunkSpawnChance < maxEmptySpawnChance)
+        {
+            emptyChunkSpawnChance += spawnChanceIncreaseAmount;
+        }
+
+        // Change the game speed
+        if (gameSpeed < maxSpeed)
+        {
+            gameSpeed += speedIncreaseAmount;
+            ChangeMoveSpeed(gameSpeed);
+        }
+
+        // Set the next time milestone
+        currentTimeMilestone += timeInterval;
+    }
+
+    void ChangeMoveSpeed(float speed)
+    {
+        moveScript.moveSpeed = originalMoveSpeed * speed;
     }
 }
